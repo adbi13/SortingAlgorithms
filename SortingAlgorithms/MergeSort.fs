725a64c6ae8da6a6bdce4a115356bd540507d0c1
adbi13 <adela.bierska@gmail.com>
2022-07-21 12:18:48 +0200

    Add MergeSort

---
+++ SortingAlgorithms/MergeSort.fs
@@ -0,0 +1,14 @@
+namespace SortingAlgorithms
+
+module MergeSort =
+    let rec merge list1 list2 =
+        match list1, list2 with
+        | list, [] | [], list -> list
+        | head1 :: tail1, head2 :: tail2 when head1 <= head2 -> head1 :: (merge tail1 (head2 :: tail2))
+        | head1 :: tail1, head2 :: tail2 -> head2 :: (merge (head1 :: tail1) tail2)
+    
+    let rec sort list =
+        match list with
+        | [] -> []
+        | [ x ] -> [ x ]
+        | _ -> merge (sort list[..(list.Length / 2)]) (sort list[(list.Length / 2)..])
