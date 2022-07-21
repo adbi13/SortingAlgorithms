5d20025b676196aeea8a323975d511eb31c7bceb
adbi13 <adela.bierska@gmail.com>
2022-07-21 12:17:40 +0200

    Add BubbleSort

---
+++ SortingAlgorithms/BubbleSort.fs
@@ -0,0 +1,18 @@
+﻿namespace SortingAlgorithms
+
+module BubbleSort =
+    let rec bubble list =
+        match list with
+        | [] -> [], false
+        | [ x ] -> [ x ], false
+        | first :: second :: tail when first > second -> bubbleNext second (first :: tail) true
+        | first :: second :: tail -> bubbleNext first (second :: tail) false
+    
+    and bubbleNext head tail swapped =
+        let tailSorted, tailSwapped = bubble tail
+        (head :: tailSorted), (swapped || tailSwapped)
+
+    let rec sort list =
+        match bubble list with
+        | sortedList, false -> sortedList
+        | unsortedList, true -> sort unsortedList
