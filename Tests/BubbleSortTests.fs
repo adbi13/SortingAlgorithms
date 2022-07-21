5b71ff60ca2aae7c5618372fa57709c4f9656c10
adbi13 <adela.bierska@gmail.com>
2022-07-21 12:17:55 +0200

    Add BubbleSort tests

---
+++ Tests/BubbleSortTests.fs
@@ -0,0 +1,48 @@
+namespace SortingAlgorithms.Tests
+
+open SortingAlgorithms
+open System
+open Xunit
+
+module BubbleSortTests =
+    [<Fact>]
+    let ``BubbleSort Empty list`` () =
+        let expected = []
+        let actual = BubbleSort.sort []
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+    [<Fact>]
+    let ``BubbleSort Single item`` () =
+        let expected = [2]
+        let actual = BubbleSort.sort [2]
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+    [<Fact>]
+    let ``BubbleSort Two items`` () =
+        let expected = [1; 2]
+        let actual = BubbleSort.sort [2; 1]
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+    [<Fact>]
+    let ``BubbleSort Sorted two items`` () =
+        let expected = [3; 9]
+        let actual = BubbleSort.sort [3; 9]
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+    [<Fact>]
+    let ``BubbleSort Short list`` () =
+        let expected = [1; 2; 3; 4; 5; 8]
+        let actual = BubbleSort.sort [4; 1; 3; 8; 2; 5]
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+    [<Fact>]
+    let ``BubbleSort Long list`` () =
+        let expected = [2; 4; 6; 13; 23; 34; 35; 54; 55; 56; 63; 92; 566; 54444487]
+        let actual = BubbleSort.sort [13; 35; 63; 2; 54; 92; 34; 55; 6; 23; 566; 54444487; 4; 56]
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+    [<Fact>]
+    let ``BubbleSort Sorted list`` () =
+        let expected = [4; 7; 9; 58; 88; 92; 120; 418; 948]
+        let actual = BubbleSort.sort [4; 7; 9; 58; 88; 92; 120; 418; 948]
+        Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
